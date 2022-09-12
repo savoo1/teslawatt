6d904085f9304fa929ee903e710ca7c2827bac91
Savo <savo@Savos-MacBook-Air-2.local>
2022-09-12 11:12:18 +0200

    new things

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ $(".navbar .lines").click(function (e) {
         $(".navbar").addClass("navbar_active");
     }
 });
+
+$(document).scroll(function() {
+  if ($(document).scrollTop() > 100) {
+      $(".navbar").addClass("nav_for_scroll");
+      $(".top_values").addClass("top_values_for_scroll");
+  }else{
+      $(".navbar").removeClass("nav_for_scroll");
+      $(".top_values").removeClass("top_values_for_scroll");
+  }
+  if ($(document).scrollTop() > 200) {
+      $(".navbar").addClass("nav_for_scroll_v2");
+      $(".top_values").addClass("top_values_for_scroll_v2");
+  }else{
+      $(".navbar").removeClass("nav_for_scroll_v2");
+      $(".top_values").removeClass("top_values_for_scroll_v2");
+  }
+  if ($(document).scrollTop() > 300) {
+      $(".navbar").addClass("nav_for_scroll_v3");
+      $(".top_values").addClass("top_values_for_scroll_v3");
+  }else{
+      $(".navbar").removeClass("nav_for_scroll_v3");
+      $(".top_values").removeClass("top_values_for_scroll_v3");
+  }
+});
